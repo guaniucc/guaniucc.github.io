ce6ecb4d4b9aa770f450bb92b6133c0a9ef15418
unknown <guaniucc@gmail.com>
2026-05-08 00:14:05 +0800

    Site updated: 2026-05-08 00:14:04

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1778170342141";const preCache = ["/images/taichi.png","/images/wutiaowu.jfif","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1778170443724";const preCache = ["/images/taichi.png","/images/wutiaowu.jfif","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
